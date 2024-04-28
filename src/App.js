d89f84ebbc54e01df7ade1fed96eae5195a8e8ae
Abhishek78912 <ak63397422@gmail.com>
2024-04-28 16:03:40 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react'
+import './home'
 
 function App() {
+  const [dataa,setdataa] = useState(0)
+  let data = 0
+  const handleincrement=()=>{
+    setdataa(dataa + 1)
+  }
+
+  const handledecrecrement=()=>{
+    setdataa(dataa-1)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   
+
+    <h1>Counter App</h1>
+       <h1>{dataa} </h1>
+  
+       <button onClick={handleincrement}>Increment</button>
+       <button onClick={handledecrecrement}>Decrement</button>
+       
     </div>
   );
 }
